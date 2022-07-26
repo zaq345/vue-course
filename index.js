1c5538a4355bb3b21e84594dcd7e08d7947489fb
Алексей Дитяткин <alexdit118@gmail.com>
2022-07-27 01:42:28 +0400

    task_3: сделано добавление новых задач

--- index.js
+++ index.js
@@ -9,12 +9,22 @@ var app = new Vue({
       {id: 3, text: 'task 3', done: true},
       {id: 4, text: 'task 4', done: false},
       {id: 5, text: 'task 5', done: false},
-    ]
+    ],
+    task: ""
   },
 
   methods: {
     itemButtonText(id){
       return this.todoItems[id-1].done ? 'completed' : 'in order';
+    },
+
+    addTask(){
+      this.todoItems.push({
+        id: this.todoItems.length + 1,
+        text: this.task,
+        done: false
+      });
+      this.task = "";
     }
   },
 
