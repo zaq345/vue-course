557057d7d1a63a109f1cf4724204d97eb11198b7
Алексей Дитяткин <alexdit118@gmail.com>
2022-07-27 00:50:59 +0400

    task_2: Выведен список задач

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ var app = new Vue({
 
   data: {
     message: "Hello from Vue App",
+    todoItems: [
+      {id: 1, text: 'task 1', done: false},
+      {id: 2, text: 'task 2', done: false},
+      {id: 3, text: 'task 3', done: true},
+      {id: 4, text: 'task 4', done: false},
+      {id: 5, text: 'task 5', done: false},
+    ]
   },
 
   methods: {
-
+    itemButtonText(id){
+      return this.todoItems[id-1].done ? 'completed' : 'in order';
+    }
   },
 
   computed: {
