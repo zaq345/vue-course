fe202cb2b5452f01843d55d8a02422a7b25d4193
Алексей Дитяткин <alexdit118@gmail.com>
2022-07-29 01:41:29 +0400

    task_8: убрано отображение изначальных задач

--- index.js
+++ index.js
@@ -37,11 +37,11 @@ var app = new Vue({
   data: {
     message: "Hello from Vue App",
     todoItems: [
-      {id: 1, text: 'task 1', done: false},
-      {id: 2, text: 'task 2', done: false},
-      {id: 3, text: 'task 3', done: true},
-      {id: 4, text: 'task 4', done: false},
-      {id: 5, text: 'task 5', done: false},
+      // {id: 1, text: 'task 1', done: false},
+      // {id: 2, text: 'task 2', done: false},
+      // {id: 3, text: 'task 3', done: true},
+      // {id: 4, text: 'task 4', done: false},
+      // {id: 5, text: 'task 5', done: false},
     ],
     task: "",
     search: "",
