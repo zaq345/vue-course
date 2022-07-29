c82c7b1c162969d6a1f65cc6694fecf87f6a6c0b
Алексей Дитяткин <alexdit118@gmail.com>
2022-07-29 23:20:41 +0400

    исправления

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ Vue.component('statistics', {
 
   computed: {
     percentage(){
-      return (this.done / this.all).toFixed(2) * 100 + '%'
+      return (this.done / this.all * 100).toFixed()  + '%'
     }
   },
 
@@ -30,6 +30,63 @@ Vue.component('statistics', {
   `
 });
 
+Vue.component('tasklist', {
+  // props: {
+  //   list: Array
+  // },
+  props: ["list", "picked", "search"],
+
+  computed: {
+    todoSort() {
+      return this.list.filter(item => item.text.indexOf(this.search) !== -1)
+    },
+  },
+
+  methods: {
+    show(done){
+
+      if (done == false && (this.picked == 1 || this.picked == 3)){
+        return true
+      }
+      if (done == true && (this.picked == 1 || this.picked == 2)){
+        return true
+      }
+    },
+
+    itemButtonText(id){
+      return this.list[id-1].done ? 'completed' : 'in order';
+    },
+  },
+
+  template: 
+  `
+    <ul class="taskList">
+
+      <li class="zeroBlock" v-if="this.list.length == 0">
+        <p class="zeroBlock__text">Please add new task</p>
+      </li>
+      
+      <li class="task" 
+            v-bind:id="item.id" 
+            v-for="(item) in todoSort"
+            v-if="show(item.done)"
+      >
+
+        <button class="task__button"
+                v-bind:class="{done_true:item.done, done_false:!item.done}"
+                v-on:click="item.done = !item.done"
+
+        >
+          {{itemButtonText(item.id)}}
+        </button>
+        <p class="task__text">{{item.text}}</p>
+      
+      </li>
+
+    </ul>
+  `
+});
+
 
 var app = new Vue({ 
   el: '#app',
@@ -37,21 +94,21 @@ var app = new Vue({
   data: {
     message: "Hello from Vue App",
     todoItems: [
-      // {id: 1, text: 'task 1', done: false},
-      // {id: 2, text: 'task 2', done: false},
-      // {id: 3, text: 'task 3', done: true},
-      // {id: 4, text: 'task 4', done: false},
-      // {id: 5, text: 'task 5', done: false},
+      {id: 1, text: 'task 1', done: false},
+      {id: 2, text: 'task 2', done: false},
+      {id: 3, text: 'task 3', done: true},
+      {id: 4, text: 'task 4', done: false},
+      {id: 5, text: 'task 5', done: false},
     ],
     task: "",
     search: "",
-    picked: "",
+    picked: "1",
   },
 
   methods: {
-    itemButtonText(id){
-      return this.todoItems[id-1].done ? 'completed' : 'in order';
-    },
+    // itemButtonText(id){
+    //   return this.todoItems[id-1].done ? 'completed' : 'in order';
+    // },
 
     addTask(){
       this.todoItems.push({
@@ -60,25 +117,23 @@ var app = new Vue({
         done: false
       });
       this.task = "";
+
     },
 
-    show(done){
-      if (done == false){
-        if (this.picked == 1 || this.picked == 3){
-          return true
-        }
-      }
-      if (done == true){
-        if (this.picked == 1 || this.picked == 2){
-          return true
-        }
-      }
-    }
+    // show(done){
+    //   if (done == false && (this.picked == 1 || this.picked == 3)){
+    //     return true
+    //   }
+    //   if (done == true && (this.picked == 1 || this.picked == 2)){
+    //     return true
+    //   }
+    // }
   },
 
   computed: {
     buttonDisabled(){
-      return this.task.length == 0
+      // return this.task.length == 0
+      return this.task.trim().length == 0
     }, 
 
     comptededTasks(){
@@ -96,8 +151,8 @@ var app = new Vue({
     },
 
     ////////////////////////////
-    todoSort() {
-      return this.todoItems.filter(item => item.text.indexOf(this.search) !== -1)
-    },
+    // todoSort() {
+    //   return this.todoItems.filter(item => item.text.indexOf(this.search) !== -1)
+    // },
   }
 });
