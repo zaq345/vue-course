3bc5e23d5edc3b8c1a7227af7a6c410928994860
Алексей Дитяткин <alexdit118@gmail.com>
2022-08-16 13:10:21 +0400

    task_5: добавлена кнопка возврата, добавлена передача описание через query параметры, сайдбар как бы есть, но там пока что просто тестовая кнопка для генирировани тасков

--- src/views/TaskComponent.vue
+++ src/views/TaskComponent.vue
@@ -1,15 +1,56 @@
 <template>
   <div class="task">
-    <h1>Task by Id {{ $route.params.id }}</h1>
+    <nav class="taskNav">
+      <button v-on:click="randItem">test</button>
+
+    </nav>
+    <div class="taskBlock">
+      <h1 class="task__header">Task by Id {{ $route.params.id }}</h1>
+      <p class="task__description">{{ $route.query.desc }}</p>
+      <button class="returnButton" v-on:click="returnFunction">Return to TaskList</button>  
+    </div>
+
   </div>
 </template>
 
 <script>
 import isAuthorised from '@/mixins/isAuthorised.vue';
+import router from '@/router';
 
 export default{
   name: 'TaskListComponent',
 
   mixins: [isAuthorised],
+
+  methods: {
+    returnFunction(){
+      router.push({path: '/taskList'});
+    },
+    randItem(){
+      let num = Math.ceil(Math.random()*1000)
+      router.push({path: '/task/'+num, query: { desc: 'Some description of task id' + num }});
+    }
+  }
 }
 </script>
+
+<style scoped>
+.task{
+  display: flex;
+}
+.taskNav{
+  min-width: 300px;
+  border-right: 1px solid gray;
+}
+.taskBlock{
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  justify-content: center;
+}
+.returnButton{
+  border: 1px solid grey;
+  border-radius: 10px;
+  padding: 10px;
+}
+</style>
